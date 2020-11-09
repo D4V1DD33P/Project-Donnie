e49fb142c4b78eefa5d0277fbdf45c4aa2344dd2
D4V1DD33P <201404972@post.au.dk>
2020-11-09 17:24:58 +0100

    Second wave

--- InteReact/Persistence/DataContext.cs
+++ InteReact/Persistence/DataContext.cs
@@ -1,27 +1,29 @@
-﻿using System;
+﻿  
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-            
         }
-
+        
         public DbSet<Value> Values { get; set; }
-
         public DbSet<Activity> Activities { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder) 
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Value>().HasData(
-                new Value {Id = 1, Name="Value Wololo 1"},
-                new Value {Id = 2, Name="Value Wololo 2"},
-                new Value {Id = 3, Name="Value Wololo 3"}
-            );
+            base.OnModelCreating(builder);
+
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "Value 101"},
+                    new Value {Id = 2, Name = "Value 102"},
+                    new Value {Id = 3, Name = "Value 103"}
+                );
         }
     }
 }
